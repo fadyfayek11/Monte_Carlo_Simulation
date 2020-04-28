b0da1d587fe16f20ecc6a7ac051befacebe3defd
Fady Fayek <Fady Fayek@DESKTOP-2GNJHSL>
2020-04-28 15:21:35 +0200

    helloagain

--- src/monte_carlo_simulation/Monte_Carlo_Simulation.java
+++ src/monte_carlo_simulation/Monte_Carlo_Simulation.java
@@ -1,15 +1,32 @@
 package monte_carlo_simulation;
 
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Monte_Carlo_Simulation {
 
     public static void main(String[] args) {
-        int [] rand;
+       /* int [] rand;
         rand=getRandomNumbers(10);
         for(int i=0;i<rand.length;i++){
             System.out.println(rand[i]);
-        }
+        }*/
+       MonteGui face=new MonteGui();
+       face.setVisible(true);
+       face.setResizable(false);
+       
+       face.setDefaultCloseOperation(EXIT_ON_CLOSE); 
+       face.setLocationRelativeTo(null);
+       
+       int Frequency[]={10,20,40,60,40,30};
+       int dailyD[]={0,1,2,3,4,5};
+       int numberofdayes=10;
+        ArrayList<Integer>inv=getSimulatedDailyDemand(dailyD,Frequency,numberofdayes);
+        System.out.println("Interval"+inv);
+        System.out.println("avg:"+avrge(inv));
+        System.out.println("expected:"+EXpected(dailyD,Frequency));
         
     }
     static double[] getProbability(int Frequency[]){
@@ -41,11 +58,51 @@ public class Monte_Carlo_Simulation {
         }
         return rand ;
     }
-    static int [] getSimulatedDailyDemand(int [] dailyDemand,double[]Frequency,int numberOfDaysYouWantToSimulate){
-        
-        
-        return null;
+    static ArrayList<Integer> getSimulatedDailyDemand(int [] dailyDemand,int[]Frequency,int numberOfDaysYouWantToSimulate){
+        ArrayList<Integer> Interval = new ArrayList<Integer>();
+        double Freq[]=getCumulative(Frequency);
+        //int[]RN=getRandomNumbers(numberOfDaysYouWantToSimulate);
+        int []RN={52,37,82,69,98,96,33,50,88,90};
        
+         for (int i = 0; i < RN.length; i++) {
+             int number1=RN[i];
+             for (int j = 0; j < Freq.length; j++) {
+                 if(1<=number1&&number1<=(int)(100*Freq[0]))
+                 {
+                     int interval=dailyDemand[0];
+                     Interval.add(interval);
+                     break;
+                 }
+                 else if ((int)(100*Freq[j])<=number1&&number1<=(int)(100*Freq[j+1]))
+                 {
+                     int interval=dailyDemand[j+1];
+                     Interval.add(interval);
+
+                 }
+             }
+         }
+         return Interval;
+    }
+    static double avrge(ArrayList<Integer> interval)
+    {
+        double avg=0.0;
+        double sum=0.0;
+        for(int i=0;i<interval.size();i++)
+        {
+            sum+=interval.get(i);
+        }
+        avg=sum/interval.size();
+        return avg;
+    }
+    static double EXpected(int []da,int []freq)
+    {
+        double Freq[]=getProbability(freq);
+        double sum=0.0;
+        for (int i = 0; i < Freq.length; i++) {
+            sum+=Freq[i]*da[i];
+        }
+    
+    return sum;
     }
 }
 
